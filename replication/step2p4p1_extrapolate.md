a93e21dc65ade22c0cebef4d814e4d7ebc010ad7
hamsi <hamsanandiniradhakrishnan@gmail.com>
2023-02-06 17:55:14 -0500

    run thru step 2.4.1 extrapolation

--- replication/step2p4p1_extrapolate.md
+++ replication/step2p4p1_extrapolate.md
@@ -6,3 +6,22 @@ Also see `*_job.sh` and `*_qsub.sh`
     * this is prosp dataset
 * output folder: `/cbica/projects/csdsi/replication/data/`
 * log folder: `/cbica/projects/csdsi/replication/data/logs`
+
+How to run:
+- in `replication` folder, `bash step2p4p1_extrapolate_qsub.sh`. Copy the command to a terminal.
+
+Run thru:
+- sub="19708"
+ses="1"
+acq="HASC55_run-01"
+- Successful job log:
+`step2p4p1_extrap_sub-19708_ses-1_HASC55_run-01.o3661526`
+- download to local computer to view in MRView
+- confirmed that generated DSI images have 258 dirs (in `mrinfo`, also bvals have 258 dirs)
+
+Checks:
+- params are the same as those in MS section 2.4
+- TODO: confirm: using `3DSHORE basis function`
+
+TODO:
+- read thru the script!
--- replication/step2p4p1_extrapolate_job.sh
+++ replication/step2p4p1_extrapolate_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -S /bin/bash
 #$ -R y
-
+#$ -l h_vmem=32G
 
 source config.txt
 echo $python_full_path
@@ -10,8 +10,15 @@ sub=$1
 ses=$2
 acq=$3
 
-indir="/cbica/projects/csdsi/BIDS/qsiprep_unzipped/sub-${sub}/ses-${ses}/dwi/"
+indir="bavasub-${sub}/ses-${ses}/dwi/"
 odir="/cbica/projects/csdsi/replication/data/extrapolated/sub-${sub}/ses-${ses}/"
 # ^^ seems have to manually add `/` at the end of `indir` and `odir`
 
 $python_full_path ../code/extrapolate_full_dsi.py ${sub} ${acq} ${indir} ${odir}
+
+
+# actual call Hamsi used:
+# qsub -o ${gridlog_path}/extrapolate/sub-${sub}_ses-s${ses}_acq-${acq}.txt -N ex-${acq}_-${sub}_${ses} -pe threaded 1-2 run_python_grid.sh extrapolate_full_dsi.py $sub $ses $acq
+
+# would take <20min
+# the log file will be ~100MB!
