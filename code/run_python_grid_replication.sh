8c427acf01d001e1a057db6ae5e40a540c819e17
hamsi <hamsanandiniradhakrishnan@gmail.com>
2023-02-08 14:19:53 -0500

    copied from cubic; duplicated it for replication

---
+++ code/run_python_grid_replication.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Run a python code on the grid
+# HR February 23, 2022.
+
+#--------------------------------------------------------------------------------------
+
+
+# Setup qsub options
+#$ -V ## pass all environment variables to the job
+#$ -S /bin/bash ## shell where it will run this job
+#$ -j y ## join error output to normal output
+#$ -cwd ## Execute the job from the current working directory
+#$ -l h_vmem=64G
+
+# Log some useful stuff into that log file 
+echo Started at `date` 
+echo Running on $HOSTNAME 
+echo USER: $USER
+export OMP_NUM_THREADS=$NSLOTS
+echo NSLOTS: $NSLOTS
+echo JOB_NAME: $JOB_NAME
+echo JOB_ID: $JOB_ID
+echo JOB_SCRIPT: $JOB_SCRIPT
+script=$1
+# below is Hamsi's:
+# code_path=/cbica/projects/csdsi/cleaned_paper_analysis/code
+# replication:
+code_path="/cbica/projects/csdsi/replication/cs_dsi/code"
+
+echo Running Python Script: $script
+echo Passed Variables: $2 $3 $4 $5 $6 $7 $8 $9
+# top -b -U csdsi > /cbica/projects/csdsi/BIDS/gridlog/get_odf/${2}_${3}_memory.txt
+echo "Process Start - `date`"
+# below is Hamsi's:
+# /cbica/projects/csdsi/miniconda3/envs/flywheel/bin/python ${code_path}/$script $2 $3 $4 $5 $6 $7 $8 $9 
+# replication:
+/cbica/projects/csdsi/miniconda3/envs/replication/bin/python ${code_path}/$script $2 $3 $4 $5 $6 $7 $8 $9 
+
+echo "Process Finish - `date`"
